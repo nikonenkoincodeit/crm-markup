f9043fa980f8c58de55cc153cea427df03e1b609
nikonenkoincodeit <75611895+nikonenkoincodeit@users.noreply.github.com>
2023-08-08 23:18:54 +0300

    left sidebar animation

--- js/sidebar.js
+++ js/sidebar.js
@@ -6,4 +6,26 @@ $(document).ready(function () {
     if (!menu.length) return;
     menu.toggleClass("show");
   });
+
+  const leftSidebar = $("left-sidebar");
+  const toggleLeftSidebar = $(".toggle-sidebar");
+  let openLeftSidebar = false;
+
+  const sidebarControl = (e) =>
+    $(e.target).hasClass("toggle-sidebar") || openLeftSidebar;
+
+  toggleLeftSidebar.on("click", onToggleLeftSidebar);
+  leftSidebar.on("mouseenter", (e) => {
+    if (sidebarControl(e)) return;
+    leftSidebar.addClass("show");
+  });
+
+  leftSidebar.on("mouseleave", (e) => {
+    if (sidebarControl(e)) return;
+    leftSidebar.removeClass("show");
+  });
+
+  function onToggleLeftSidebar() {
+    openLeftSidebar = leftSidebar[0].classList.toggle("show");
+  }
 });
