34e5882b4e3fa9e578b001d8fa2c6b30f5a12ffa
nikonenkoincodeit <75611895+nikonenkoincodeit@users.noreply.github.com>
2023-08-15 15:20:03 +0300

    added hide menu

--- js/header-menu.js
+++ js/header-menu.js
@@ -1,32 +1,45 @@
 jQuery(document).ready(function ($) {
   function calcWidth() {
     var navwidth = 0;
-    var morewidth = $(".site-nav-list .more").outerWidth(true);
+    var morewidth = $(".more").outerWidth(true);
+
     $(".site-nav-list > li:not(.more)").each(function () {
       navwidth += $(this).outerWidth(true);
     });
     var availablespace = $(".site-nav-list").outerWidth(true) - morewidth;
 
-    if (navwidth < availablespace) {
-      var lastItem = $(".site-nav-list > li:not(.more)").last();
+    if (navwidth > availablespace) {
+      const list = $(".site-nav-list > li:not('.more')");
+      let lastItem = $(list[list.length - 1]);
       lastItem.attr("data-width", lastItem.outerWidth(true));
-      lastItem.prependTo($(".site-nav-list .more ul"));
+      $(".hide-list").append(lastItem.clone());
+      lastItem.remove();
 
       calcWidth();
     } else {
-      var firstMoreElement = $(".site-nav-list li.more li").first();
+      var firstMoreElement = $(".hide-list li").last();
       if (navwidth + firstMoreElement.data("width") < availablespace) {
-        firstMoreElement.insertBefore($(".site-nav-list .more"));
+        $(".site-nav-list").append(firstMoreElement);
       }
     }
-
-    if ($(".more li").length > 0) {
-      $(".more").css("display", "block");
-    } else {
-      $(".more").css("display", "none");
-    }
   }
-  // $(window).on("resize load", function () {
-  //   calcWidth();
-  // });
+  $(window).on("resize load", function () {
+    calcWidth();
+  });
+
+  $(".site-nav-list-hide").sortable({
+    handle: ".icon-drag",
+  });
+
+  const siteNavHide = $(".site-nav-hide");
+  const closeBtn = $(".close-btn");
+
+  $(".toggle-nav").on("click", (e) => {
+    if (e.target.closest(".close-btn")) return;
+    siteNavHide.removeClass("is-hidden");
+  });
+
+  closeBtn.on("click", () => {
+    siteNavHide.addClass("is-hidden");
+  });
 });
