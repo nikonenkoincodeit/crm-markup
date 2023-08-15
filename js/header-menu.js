1db66d8f8dcbcbde49dce2a9cd4894dfb86dbab5
nikonenkoincodeit <75611895+nikonenkoincodeit@users.noreply.github.com>
2023-08-15 23:05:12 +0300

    fix

--- js/header-menu.js
+++ js/header-menu.js
@@ -1,32 +1,4 @@
 jQuery(document).ready(function ($) {
-  function calcWidth() {
-    var navwidth = 0;
-    var morewidth = $(".more").outerWidth(true);
-
-    $(".site-nav-list > li:not(.more)").each(function () {
-      navwidth += $(this).outerWidth(true);
-    });
-    var availablespace = $(".site-nav-list").outerWidth(true) - morewidth;
-
-    if (navwidth > availablespace) {
-      const list = $(".site-nav-list > li:not('.more')");
-      let lastItem = $(list[list.length - 1]);
-      lastItem.attr("data-width", lastItem.outerWidth(true));
-      $(".hide-list").append(lastItem.clone());
-      lastItem.remove();
-
-      calcWidth();
-    } else {
-      var firstMoreElement = $(".hide-list li").last();
-      if (navwidth + firstMoreElement.data("width") < availablespace) {
-        $(".site-nav-list").append(firstMoreElement);
-      }
-    }
-  }
-  $(window).on("resize load", function () {
-    calcWidth();
-  });
-
   $(".site-nav-list-hide").sortable({
     handle: ".icon-drag",
   });
--- js/sidebar.js
+++ js/sidebar.js
@@ -1,33 +1,61 @@
 $(document).ready(function () {
-  const leftSidebarLink = $("left-sidebar a");
+  const menuSidebarLink = $("menu-sidebar a");
+  const menuSidebar = $("menu-sidebar");
 
-  leftSidebarLink.on("click", (e) => {
+  menuSidebarLink.on("click", (e) => {
     const menu = $(e.target).closest(".item").find(".nested-menu");
     if (!menu.length) return;
     menu.toggleClass("show");
   });
 
+  $(".tog-btn").on("click", (e) => {
+    $(e.target).toggleClass("show");
+  });
+
+  $("menu-sidebar .close-menu").on("click", (e) => {
+    menuSidebar.addClass("is-hidden");
+  });
+
+  $(".logo").on("click mouseover", (e) => {
+    menuSidebar.removeClass("is-hidden");
+  });
+
   const leftSidebar = $("left-sidebar");
   const rightSidebar = $("right-sidebar");
+  let calcWidthIndex = 0;
 
-  // const iconMenu = $(".icon-menu");
-  // const iconMenuRight = $(".icon-menu-right");
   const app = $(".app");
 
-  // const toggleLeftSidebar = leftSidebar.find(".toggle-sidebar");
   const btnToggle = $(".toggle-sidebar");
 
   const getParentInfo = (el) => {
     const parentEl = el.parent();
     const pos = parentEl[0].tagName.toLowerCase().split("-")[0];
-    const width = parentEl.attr("data-width") || 300;
+    const wEl = $(window).width() > 992 ? 300 : 400;
+    const width = parentEl.attr("data-width") || wEl;
     return { parentEl, pos, width };
   };
 
   function toggleSidebar(parentEl, pos, width, method = "toggle") {
-    const bool = parentEl[0].classList[method]("show");
-    const offset = bool ? 0 : "-" + (width - 30) + "px";
-    parentEl.css("margin-" + pos, offset);
+    const toggle = $(window).width() > 992;
+    const classEl = $(window).width() > 992 ? "show" : "hover";
+    const bool = parentEl[0].classList[method](classEl);
+
+    if (toggle) {
+      const offset = bool ? 0 : "-" + (width - 30) + "px";
+      parentEl.css("margin-" + pos, offset);
+    }
+
+    let index = 0;
+    let id = setInterval(() => {
+      if (index <= 12) {
+        calcWidthIndex = 0;
+        calcWidth();
+      } else {
+        clearInterval(id);
+      }
+      index += 1;
+    }, 100);
   }
 
   btnToggle.on("click", function () {
@@ -35,39 +63,59 @@ $(document).ready(function () {
     toggleSidebar(parentEl, pos, width);
   });
 
-  let idTimeout = null;
+  const btnSidebarRight = $(".icon-menu-right");
+  const btnSidebarLeft = $(".icon-left-sidebar");
 
-  $(".sidebar-content").hover(
-    function () {
-      const { parentEl, pos } = getParentInfo($(this));
-      if (parentEl.hasClass("show")) return;
-      app.css("margin-" + pos, "30px");
-      parentEl.addClass("hover p-fixed");
-      parentEl.css("margin-" + pos, -1 + "px");
-      clearTimeout(idTimeout);
-    },
-    function () {
-      const { parentEl, pos, width } = getParentInfo($(this));
-      if (parentEl.hasClass("show")) return;
-      parentEl.removeClass("hover");
-      console.log("width ", width);
-      parentEl.css("margin-" + pos, "-" + (width - 30) + "px");
-      idTimeout = setTimeout(function () {
-        parentEl.removeClass("p-fixed");
-
-        app.css("margin-" + pos, 0);
-      }, 1000);
-    }
-  );
-
-  const footerBtn = $(".footer-btn");
-  const conFluid = $(".container-fluid");
-
-  footerBtn.on("click", () => {
-    rightSidebar.toggleClass("hide-element");
-    leftSidebar.toggleClass("hide-element");
-    conFluid.toggleClass("fluid");
+  btnSidebarRight.on("click", function () {
+    const { parentEl, pos, width } = getParentInfo(
+      $("right-sidebar .toggle-sidebar")
+    );
+    toggleSidebar(parentEl, pos, width);
   });
+
+  btnSidebarLeft.on("click", function () {
+    const { parentEl, pos, width } = getParentInfo(
+      $("left-sidebar .toggle-sidebar")
+    );
+    toggleSidebar(parentEl, pos, width);
+  });
+
+  if ($(window).width() > 992) {
+    let idTimeout = null;
+
+    $(".sidebar-content").hover(
+      function (e) {
+        if (e.target.closest(".coll-btn")) return;
+        const { parentEl, pos } = getParentInfo($(this));
+        if (parentEl.hasClass("show")) return;
+        app.css("margin-" + pos, "30px");
+        parentEl.addClass("hover p-fixed");
+        parentEl.css("margin-" + pos, -1 + "px");
+        clearTimeout(idTimeout);
+      },
+      function (e) {
+        if (e.target.closest(".coll-btn")) return;
+        const { parentEl, pos, width } = getParentInfo($(this));
+        if (parentEl.hasClass("show")) return;
+        parentEl.removeClass("hover");
+        console.log("width ", width);
+        parentEl.css("margin-" + pos, "-" + (width - 30) + "px");
+        idTimeout = setTimeout(function () {
+          parentEl.removeClass("p-fixed");
+
+          app.css("margin-" + pos, 0);
+        }, 1000);
+      }
+    );
+  }
+  // const footerBtn = $(".footer-btn");
+  // const conFluid = $(".container-fluid");
+
+  // footerBtn.on("click", () => {
+  //   rightSidebar.toggleClass("hide-element");
+  //   leftSidebar.toggleClass("hide-element");
+  //   conFluid.toggleClass("fluid");
+  // });
 
   const resizeSidebar = $(".resize-sidebar");
 
@@ -90,6 +138,8 @@ $(document).ready(function () {
     }
     el[0].style.width = width + "px";
     el.attr("data-width", width);
+    calcWidthIndex = 0;
+    calcWidth();
   });
 
   window.addEventListener("mouseup", stopResize);
@@ -107,4 +157,51 @@ $(document).ready(function () {
     );
     toggleSidebar(parentEl, pos, width, "remove");
   }
+
+  const closeSidebarLeft = $("left-sidebar .close-menu");
+  const closeSidebarRight = $("right-sidebar .close-menu");
+
+  closeSidebarLeft.on("click", (e) => {
+    const { parentEl, pos, width } = getParentInfo($(e.currentTarget));
+    toggleSidebar(parentEl, pos, width, "remove");
+  });
+
+  closeSidebarRight.on("click", (e) => {
+    const { parentEl, pos, width } = getParentInfo($(e.currentTarget));
+    toggleSidebar(parentEl, pos, width, "remove");
+  });
+
+  function calcWidth() {
+    if ($(window).width() <= 576) return;
+    var navwidth = 0;
+    var morewidth = $(".more").outerWidth(true);
+
+    $(".site-nav-list > li:not(.more)").each(function (e) {
+      navwidth += $(this).outerWidth(true);
+    });
+
+    var availablespace = $(".site-nav-list").outerWidth(true) - morewidth;
+
+    if (navwidth > availablespace) {
+      const list = $(".site-nav-list > li:not('.more')");
+      let lastItem = $(list[list.length - 1]);
+      lastItem.attr("data-width", lastItem.outerWidth(true));
+      $(".hide-list").append(lastItem.clone());
+      lastItem.remove();
+
+      if (calcWidthIndex <= 10) {
+        calcWidth();
+      }
+      calcWidthIndex += 1;
+    } else {
+      var firstMoreElement = $(".hide-list li").last();
+      if (navwidth + firstMoreElement.data("width") < availablespace) {
+        $(".site-nav-list").append(firstMoreElement);
+      }
+    }
+  }
+  $(window).on("resize load", function () {
+    calcWidthIndex = 0;
+    calcWidth();
+  });
 });
